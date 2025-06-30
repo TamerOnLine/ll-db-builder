416d500c0203651fd7324fc156264de7915816ae
TamerOnLine <tamer@tameronline.com>
2025-07-01 01:22:39 +0200

    a01

---
+++ main/models/__init__.py
@@ -0,0 +1,26 @@
+from main.extensions import db
+
+from .resume_section import ResumeSection
+from .resume_paragraph import ResumeParagraph
+from .resume_field import ResumeField
+from .resume_setting import Setting
+from .Section import Section
+from .LanguageOption import LanguageOption
+from .NavigationLink import NavigationLink
+from .sidebar_link import SidebarLink  # تأكد من استيراد SidebarLink إذا كان موجودًا
+
+
+# تأكد من استيراد db فقط إذا كان ذلك منطقيًا في السياق
+# من مكان تعريف db:
+# from main import db
+
+__all__ = [
+    "ResumeSection",
+    "ResumeParagraph",
+    "ResumeField",
+    "Setting",
+    "Section",
+    "LanguageOption",
+    "NavigationLink",
+    "SidebarLink"
+]
